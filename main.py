223c5fd2288f854b7b8b5146339b4964ee92481c
Andrii Lunin <alunin@palantir.com>
2023-02-03 13:36:55 +0000

    fix match case statement

--- main.py
+++ main.py
@@ -3,39 +3,35 @@ import formatters
 def main():
     prev_output = ""
 
-    availiable_formatters = ["plain", "bold", "italic", "inline-code", "header", "link", "ordered-list", "unordered-list"]
+    available_formatters = ["plain", "bold", "italic", "inline-code", "header", "link", "ordered-list", "unordered-list"]
 
     while True:
         print("Choose one of the formatters: plain, bold, italic, inline-code, header, link, ordered-list, unordered-list")
-        default_question = str(input("Type a formatter of choice or type 'finish' (to exit a program and save the ouput):"))
-        if default_question == "finish":
-            break
-        elif default_question in availiable_formatters:
+        formatter = input("Type a formatter of choice or type 'finish' (to exit a program and save the output):")
 
-            formatter = default_question
-
-            match formatter:
-
-                case "plain":
-                    prev_output = formatters.text_markdown(formatter, prev_output)
-                case "bold":
-                    prev_output = formatters.text_markdown(formatter, prev_output)
-                case "italic":
-                    prev_output = formatters.text_markdown(formatter, prev_output)
-                case "inline-code":
-                    prev_output = formatters.text_markdown(formatter, prev_output)
-                case "header":
-                    prev_output = formatters.header_markdown(formatter, prev_output)
-                case "link":
-                    prev_output = formatters.link_markdown(formatter, prev_output)
-                case "new-line":
-                    prev_output = formatters.new_line_markdown(formatter, prev_output)
-                case "ordered-list":
-                    prev_output = formatters.list_markdown(formatter, prev_output)
-                case "unordered-list":
-                    prev_output = formatters.list_markdown(formatter, prev_output)
-        else:
-            print("Unknown formatting type or command")
+        match formatter:
+            case "finish":
+                break
+            case "plain":
+                prev_output = formatters.text_markdown(formatter, prev_output)
+            case "bold":
+                prev_output = formatters.text_markdown(formatter, prev_output)
+            case "italic":
+                prev_output = formatters.text_markdown(formatter, prev_output)
+            case "inline-code":
+                prev_output = formatters.text_markdown(formatter, prev_output)
+            case "header":
+                prev_output = formatters.header_markdown(formatter, prev_output)
+            case "link":
+                prev_output = formatters.link_markdown(formatter, prev_output)
+            case "new-line":
+                prev_output = formatters.new_line_markdown(formatter, prev_output)
+            case "ordered-list":
+                prev_output = formatters.list_markdown(formatter, prev_output)
+            case "unordered-list":
+                prev_output = formatters.list_markdown(formatter, prev_output)
+            case _:
+                print("Unknown formatting type or command")
 
         print(prev_output)
 
