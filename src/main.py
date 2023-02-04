2b50e31f97f419ff26e774875104741d947216d3
ELvira Selivanova <es5332@nyu.edu>
2023-02-04 21:58:23 +0400

    putting output code in separate file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import utils.formatters as formatters
-import os
+import utils.output as output
 
 def main():
     prev_output = ""
@@ -35,35 +35,7 @@ def main():
 
         print(prev_output)
 
-    while True:
-
-        if len(os.listdir("src/outputs")) == 0:
-            name = input("Type the name of an output file in a text format without spaces:")
-            mode = "w"
-            break
-        else:
-            print("Do you want to create a new file with a text or append to the existing one?")
-            question = input("Type 'new' or 'append':")
-            if question == "new":
-                name = input("Type the name of an output file in a text format without spaces:")
-                mode = "w"
-                break
-            elif question == "append":
-                available_files = os.listdir("src/outputs")
-                name = None
-                while name not in available_files:
-                    print(f"Those are the available files in 'outputs' folder: {available_files}")
-                    name = input("Type the name of an file from a given choice:")
-                    name = name.split(".md")[0]
-                mode = "a"
-                break
-            else:
-                print("Unknown command. Please choose between 'new' and 'append'.")
-
-    # saving output 
-    file = open(f'src/outputs/{name}.md', mode, encoding='utf-8')
-    file.writelines(prev_output)
-    file.close()
+    output.output_file(prev_output)
 
 if __name__ == "__main__":
     main()
